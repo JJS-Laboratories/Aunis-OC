a53fe5a58cd277d053dc4b4629010b84e5eae9cf
Jaja Steele <superjaja05@gmail.com>
2023-02-07 07:14:58 +0100

    Create rs_ring.lua

---
+++ rs_ring.lua
@@ -0,0 +1,66 @@
+local component = require("component")
+local ring = component.transportrings
+local gpu = component.gpu
+local rs = component.redstone
+local term = require("term")
+local fs1 = require("filesystem")
+local srz = require("serialization")
+local event = require("event")
+
+local function split(s, delimiter)
+    local result = {};
+    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
+
+local function fs_text(txt,fg,bg)
+    term.clear()
+    term.setCursor(1,1)
+    gpu.setResolution(txt:len(),1)
+    gpu.setForeground(fg or 0xFFFFFF)
+    gpu.setBackground(bg or 0x000000)
+    term.write(txt)
+end
+
+local config = {}
+
+if not fs1.exists("/home/rs_ring.txt") then
+    print("Config Mode..")
+    print("Type of Ring? (GOAULD, ORI)")
+    config.type = io.read()
+    if config.type == "GOAULD" then config.type = 0 end
+    if config.type == "ORI" then config.type = 1 end
+    print("Target Address?")
+    config.target = {}
+    for i1=1, 4 do
+        config.target[i1] = io.read()
+    end
+    local config_file = io.open("/home/rs_ring.txt","w")
+    config_file:write(srz.serialize(config))
+    config_file:close()
+else
+    print("Loading Config..")
+    local config_file = io.open("/home/rs_ring.txt","r")
+    config = srz.unserialize(config_file:read("*a"))
+    config_file:close()
+end
+
+while true do
+    fs_text("Idle")
+    local _, _, _, old, new = event.pull("redstone_changed")
+    if old == 0 and new > 0 then
+        for k,v in ipairs(config.target) do
+            if v:match("%a") then
+                fs_text(v)
+                ring.addSymbolToAddress(0,v)
+            end
+            os.sleep(1)
+        end
+        fs_text("Dialing",0x44FF66)
+        ring.addSymbolToAddress(0,6)
+    end
+end
+
+component.gpu.setResolution(component.gpu.maxResolution())
